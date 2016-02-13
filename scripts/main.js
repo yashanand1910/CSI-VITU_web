b4aae64a7487c03bacbe816f780e8e20c2842766
yasharora404 <yasharora.arora404@gmail.com>
2016-02-14 01:02:32 +0530

    Added the project section, event section and profile section

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,18 @@
+    $("#profile_gallery").bootslider({
+    snapToItem: true
+    });
+
+$("#profile_gallery > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#profile_gallery > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#profile_gallery');
+},  3000);
+
 
     $(".burger-menu").click(function () {
 	$(this).toggleClass("menu-on");
