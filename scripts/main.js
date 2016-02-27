8b7c6317282f8ccac762f2769f9dabd1ab0edccd
yasharora404 <yasharora.arora404@gmail.com>
2016-02-27 11:03:23 +0530

    Added svg, new nav

--- scripts/main.js
+++ scripts/main.js
@@ -14,4 +14,25 @@ setInterval(function() {
 
     $(".burger-menu").click(function () {
 	$(this).toggleClass("menu-on");
-	});                 
+	});   
+
+//smooth scrolling
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 800);
+        return false;
+      }
+    }
+  });
+});
+
+$("a[href='#top']").click(function() {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  return false;
+});
